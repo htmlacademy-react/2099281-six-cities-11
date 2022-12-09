c9caa15291a53f43ff6a0c1dc6efede518877ade
Sergey Sytsevich <sytsevichs@ya.ru>
2022-12-09 03:23:21 +0300

    7.1 Отладка 1

--- project/src/types/types.ts
+++ project/src/types/types.ts
@@ -1,59 +1,59 @@
 export type LocationType = {
-  latitude: number
-  longitude: number
-  zoom: number
+  latitude: number;
+  longitude: number;
+  zoom: number;
 }
 
 export type CityType = {
-  location: LocationType
-  name: string
+  location: LocationType;
+  name: string;
 }
 
 export type HostType = {
-  avatarUrl: string
-  id: number
-  isPro: boolean
-  name: string
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
 }
 
 export type OfferType = {
-  bedrooms: number
-  city: CityType
-  description: string
-  goods: [string]
-  host: HostType
-  id: number
-  images: [string]
-  isFavorite: boolean
-  isPremium: boolean
-  location: LocationType
-  maxAdults: number
-  previewImage: string
-  price: number
-  rating: number
-  title: string
-  type: string
+  bedrooms: number;
+  city: CityType;
+  description: string;
+  goods: [string];
+  host: HostType;
+  id: number;
+  images: [string];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: LocationType;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
 }
 
 export type OffersType = OfferType[];
 
 export type CommentType = {
-  comment: string
-  date: string
-  id: number
-  rating: number
-  user: HostType
+  comment: string;
+  date: string;
+  id: number;
+  rating: number;
+  user: HostType;
 }
 
 export type CommentsType = CommentType[];
 
 export type UserType = {
-  avatarUrl: string
-  email: string
-  id: number
-  isPro: boolean
-  name: string
-  token: string
+  avatarUrl: string;
+  email: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+  token: string;
 };
 
 export type AuthData = {
